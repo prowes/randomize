488a846b0a6052b22bc26fe747ab68ab7a5255e8
ivan prowes <locust94@gmail.com>
2022-02-06 20:39:21 +0200

    Squash into one file (needed for Beanstalk)

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField
-from wtforms.validators import DataRequired
-
-class EnterForm(FlaskForm):
-    down_border = IntegerField('From', validators=[DataRequired()])
-    up_border = IntegerField('To', validators=[DataRequired()])
-    submit = SubmitField('Randomize')
